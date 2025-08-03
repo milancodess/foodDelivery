a3cba05345b77da91d31590d89c5f9edb30dc39d
milancodess <milanbhandari153@gmail.com>
2025-08-03 14:14:39 +0545

    fix not showing image

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,20 @@
 import React from "react";
+import headerImg from "../../assets/header_img.png";
 import "./Header.css";
 
 const Header = () => {
   return (
-    <div className="header">
+    <div
+      className="header"
+      style={{
+        backgroundImage: `url(${headerImg})`,
+        backgroundRepeat: "no-repeat",
+        backgroundSize: "contain",
+        height: "34vw",
+        margin: "30px auto",
+        position: "relative",
+      }}
+    >
       <div className="header-contents">
         <h2>Order your favorite food here.</h2>
         <p>
