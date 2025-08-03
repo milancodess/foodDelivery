3bc0def58a51232a0bb0be10cc32c0bd6b38b56b
milancodess <milanbhandari153@gmail.com>
2025-08-03 13:52:40 +0545

    add links to social

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -4,10 +4,10 @@ import { assets } from "../../assets/assets";
 
 const Footer = () => {
   return (
-    <div className="footer" id="footer">
+    <footer className="footer" id="footer">
       <div className="footer-content">
-        <div className="footer-content-left">
-          <img src={assets.logo} alt="Logo" />
+        <section className="footer-content-left">
+          <img src={assets.logo} alt="Tomato Logo" />
           <p>
             Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quaerat
             velit iure atque dolore iusto ducimus aperiam dicta magni totam
@@ -15,11 +15,32 @@ const Footer = () => {
             suscipit?
           </p>
           <div className="footer-social-icons">
-            <img src={assets.facebook_icon} alt="Facebook" />
-            <img src={assets.twitter_icon} alt="Twitter" />
-            <img src={assets.linkedin_icon} alt="LinkedIn" />
+            <a
+              href="https://facebook.com/milancodess"
+              aria-label="Facebook"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img src={assets.facebook_icon} alt="Facebook" />
+            </a>
+            <a
+              href="https://twitter.com/milancodess"
+              aria-label="Twitter"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img src={assets.twitter_icon} alt="Twitter" />
+            </a>
+            <a
+              href="https://www.linkedin.com/in/milancodess/"
+              aria-label="LinkedIn"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img src={assets.linkedin_icon} alt="LinkedIn" />
+            </a>
           </div>
-        </div>
+        </section>
 
         <div className="footer-content-center">
           <h2>COMPANY</h2>
@@ -31,20 +52,24 @@ const Footer = () => {
           </ul>
         </div>
 
-        <div className="footer-content-right">
+        <section className="footer-content-right">
           <h2>GET IN TOUCH</h2>
           <ul>
-            <li>+977 9812345678</li>
-            <li>milan@tomato.com</li>
+            <li>
+              <a href="tel:+9779812345678">+977 9812345678</a>
+            </li>
+            <li>
+              <a href="mailto:githubmilan@gmail.com">githubmilan@gmail.com</a>
+            </li>
           </ul>
-        </div>
+        </section>
       </div>
 
       <hr />
       <p className="footer-copyright">
         &copy; 2025 Tomato.com — All Rights Reserved.
       </p>
-    </div>
+    </footer>
   );
 };
 
